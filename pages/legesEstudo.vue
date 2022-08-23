3dc92fe20818a3aa8aea9f8ada52646dd6459d61
Joao Humberto <juninho.joao@hotmail.com>
2022-08-23 11:27:02 -0300

    landingpage

--- pages/legesEstudo.vue
+++ pages/legesEstudo.vue
@@ -3,13 +3,13 @@
             <v-app-bar flat color="transparent">
                 <div class="d-flex">
                     <v-icon color="indigo" large class="mr-1">mdi-sun-compass</v-icon>
-                    <h5 class="text-h5 leges">Estudo da Lei</h5>
+                    <h5 to="/" class="text-h5 leges">Estudo da Lei</h5>
                 </div>
                 <v-spacer></v-spacer>
-                <v-btn text>Início</v-btn>
-                <v-btn text class="mx-2">Súmulas</v-btn>
-                <v-btn text>Julgados</v-btn>
-                <v-btn text class="ml-2" rounded color="indigo" outlined>Login</v-btn>
+                <v-btn to="/" text>Início</v-btn>
+                <v-btn to="/sumulas" text class="mx-2">Súmulas</v-btn>
+                <v-btn to="/juris" text>Julgados</v-btn>
+                <v-btn to="/login" text class="ml-2" rounded color="indigo" outlined>Login</v-btn>
             </v-app-bar>
         <v-card flat class="my-16 pt-16">
             <v-row class="mt-16">
@@ -30,6 +30,27 @@
                     lazy-src="/landingPage/telaLeges.png"
                     max-height="500"
                 ></v-img>
+            </v-card-text>
+        </v-card>
+        <v-card flat class="my-16">
+            <v-card-title class="description"> <span class="tag mr-3 indigo"></span> O que é o Estudo da Lei?</v-card-title>
+            <v-card-text class="text">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit libero ad similique consequatur 
+                    error tenetur nam pariatur dolorum inventore dolor laborum, omnis corporis dolore iste nobis ratione eveniet ducimus iure!
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit libero ad similique consequatur 
+                    error tenetur nam pariatur dolorum inventore dolor laborum, omnis corporis dolore iste nobis ratione eveniet ducimus iure!
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit libero ad similique consequatur 
+                    error tenetur nam pariatur dolorum inventore dolor laborum, omnis corporis dolore iste nobis ratione eveniet ducimus iure!
+                </p>
+            </v-card-text>
+         </v-card>
+        <funcionalidades />
+        <v-card flat class="my-16">
+            <v-card-text class="text-center">
+                <p class="footer">Desenvolvido e idealizado por <span class="indigo--text font-weight-bold">João Humberto Silva Ribeiro Júnior</span> </p>
+                <v-btn class="mt-n6" text href="https://www.instagram.com/humbertojhj/"> <v-icon class="mr-1">mdi-instagram</v-icon> @humbertojhj</v-btn>
             </v-card-text>
         </v-card>
     </v-container>
@@ -71,6 +92,23 @@
 }
 .description {
     font-size: 2.2rem;
+    line-height: 3rem;
+    font-weight: 400;
+}
+.text{
+    font-family: Poppins, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;;
+    display: block;
+    font-size: 1.8rem;
+    line-height: 2.5rem;
+}
+
+.tag{
+    height: 50px;
+    width: 20px;
+}
+
+.footer {
+    font-size: 1.2rem;
     line-height: 3rem;
     font-weight: 400;
 }
